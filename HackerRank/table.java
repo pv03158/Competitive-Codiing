40245d33ff5ac41f22fea1da5aac56a31b286779
Prashant Varshney <pv03158@gmail.com>
2019-11-25 20:34:49 +0530

    for loop in java and output formatting

---
+++ HackerRank/table.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class table {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i = 1; i <= 10; i++){
+            System.out.printf("%d x %d = %d\n",n,i,n*i);
+        }
+        input.close();
+    }
+}
