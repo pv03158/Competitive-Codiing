23e87b94971839fa418daa3d4211d96689aa0041
Prashant Varshney <pv03158@gmail.com>
2019-12-10 22:26:37 +0530

    practising java

---
+++ Practise/Sorting.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class Sorting{
+    // declaring global variables
+    int arr[];
+
+   // declaring global functions
+    private static readFile(String fileName){
+        File inputFile = new File(fileName);
+        Scanner fin = new Scanner(inputFile);
+    }
+
+   // driver function
+    public static void main(String args[]){
+        
+    }
+}
