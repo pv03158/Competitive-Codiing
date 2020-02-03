ca3e2a526f75d1c18aa8ff80a6b4f7731e65a597
Prashant Varshney <pv03158@gmail.com>
2020-02-03 21:17:46 +0530

    added pyramid pattern

--- HackerRank/pyramidPattern.py
+++ HackerRank/pyramidPattern.py
@@ -1,2 +1,18 @@
 n = int(input())
-for i in range()
+pos = 1
+flag = False
+for i in range(-n//2 + 1,n//2 + 1):
+    #printing spaces
+    for _ in range(abs(i)):
+        print(' ',end='')
+    #printing numbers
+    for j in range(pos,n-2*(abs(i))+1):
+        print(j,end='')
+    #printing in reverse order
+    for k in range(n-2*(abs(i))-1,pos-1,-1):
+        print(k,end='')
+    if i < 0:
+        pos += 1
+    else:
+        pos -= 1
+    print()
