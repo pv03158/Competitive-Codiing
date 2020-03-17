fa569b9dace3f7f969f4fb0639e301a8ffed186a
Prashant Varshney <pv03158@gmail.com>
2020-03-17 23:59:54 +0530

    started doing codechef problems

---
+++ CodeChef/IsThisALongChallenge.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class IsThisALongChallenge{
+    public static Scanner scn = new Scanner(System.in);
+    public static void main(String args[]){
+        System.out.println(scn.nextInt());
+    }
+}
