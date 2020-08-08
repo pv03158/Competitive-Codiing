923408a67f1ec97607c422ce3b4ea23916254591
Prashant Varshney <pv03158@gmail.com>
2020-08-08 07:53:26 +0530

    solved problem - decode ways with memoization(91)

--- Leetcode/DecodeWays.java
+++ Leetcode/DecodeWays.java
@@ -1,23 +1,30 @@
+// LEETCODE PROBLEM - 91 DECODE WAYS
+
 class Solution {
     public int numDecodings(String str) {
-        if(str.length() == 0)
+        return solve(str,0,new int[str.length()]);
+    }
+    
+    public int solve(String str, int cidx, int dp[]) {
+        if(cidx == str.length())
             return 1;
         
+        if(dp[cidx] != 0)
+            return dp[cidx];
+        
         int counter = 0;
-        if(str.length() >= 1) {
-            int val = Integer.valueOf(str.substring(0,1));
-            if(val == 0)
-                return 0;
+        if( cidx + 1 <= str.length() ) {
+            int val = Integer.valueOf(str.substring(cidx,cidx+1));
             if(val >= 1 && val <= 9)
-                counter += numDecodings(str.substring(1));
+                counter += solve(str,cidx+1,dp);
         }
-        if(str.length() >= 2) {
-            int val = Integer.valueOf(str.substring(0,2));
-            if(val == 0)
-                return 0;
+        if( cidx + 2 <= str.length() ) {
+            int val = Integer.valueOf(str.substring(cidx, cidx+2));
             if(val >= 10 && val <= 26)
-                counter += numDecodings(str.substring(2));
+                counter += solve(str,cidx+2,dp);
         }
-        return counter;
+        
+        dp[cidx] = counter;
+        return dp[cidx];
     }
 }
