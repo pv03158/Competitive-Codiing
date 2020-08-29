2072a53e1df1430b5f6bfa9deff55fca6939aa56
Prashant Varshney <pv03158@gmail.com>
2020-08-29 22:36:55 +0530

    solved problem Merge K Sorted Lists using Divide and Conquer Algorithm

--- Leetcode/MergeKSortedLists.java
+++ Leetcode/MergeKSortedLists.java
@@ -48,3 +48,52 @@ class Solution {
         return dummy.next;
     }
 }
+
+// APPROACH -2 DIVIDE & CONQUER
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0)
+            return null;
+        return helper(lists, 0 , lists.length - 1);
+    }
+    
+    private ListNode helper(ListNode[] lists, int lb, int ub)  {
+        if(lb == ub)
+            return lists[ub];
+        
+        int mid = (lb + ub) / 2;
+        return merge(helper(lists, lb, mid), helper(lists, mid + 1, ub));
+    }
+    
+    private ListNode merge(ListNode h1, ListNode h2) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = null;
+        ListNode ptr = dummy;
+        
+        while(h1 != null && h2 != null) {
+            if(h1.val <= h2.val) {
+                ptr.next = h1;
+                h1 = h1.next;
+            }
+            else {
+                ptr.next = h2;
+                h2 = h2.next;
+            }
+            ptr = ptr.next;
+        }
+        
+        if(h1 == null) ptr.next = h2;
+        else ptr.next = h1;
+        return dummy.next;
+    }
+}
