086daf08be1b72868e419edc8c973b044f4fa66c
Prashant Varshney <pv03158@gmail.com>
2020-09-25 14:49:25 +0530

    solved problem - Island Perimeter using IInd Approach

--- Leetcode/IslandPerimeter.java
+++ Leetcode/IslandPerimeter.java
@@ -1,3 +1,29 @@
+// APPROACH -- 1
+class Solution {
+    public int islandPerimeter(int[][] arr) {
+        int land = 0;
+        int edge = 0;
+        
+        int m = arr.length;
+        int n = arr[0].length;
+        
+        for(int i = 0 ; i < m ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                if(arr[i][j] == 1) {
+                    land ++;
+                    if(i > 0 && arr[i-1][j] == 1)
+                        edge ++;
+                    if(j > 0 && arr[i][j-1] == 1)
+                        edge ++;
+                }
+            }
+        }
+        
+        return land*4 - edge*2;
+    }
+}
+
+// APPROACH -- 2
 class Solution {
     public int islandPerimeter(int[][] grid) {
         if(grid.length == 0 || grid[0].length == 0)
