2a5ed5261e8faa85990faf923b260e2779f3fa70
Prashant Varshney <pv03158@gmail.com>
2020-09-28 19:42:25 +0530

    solved problem password

--- Codeforces/Password.cpp
+++ Codeforces/Password.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-int 
-main(int argc, char* argv[]) {
-    string str;
-    cin >> str;
+int
+main(int argc , char* argv[]) {
+    string text;
+    cin >> text;
 
-    int length = str.length();
-    const int base = 2;
-    const int64_t mod = 1e8 + 7;
-    int64_t p[ length ];
-    // GENERATING MULTIPLIER
+    // GENERATING PREFIX SUM VECTOR OF STRING HASH
+    const int base = 107;
+    const int mod = 1e9 + 7;
+    // HASHING FORMULA - (a + b*p1 + c*p2 + ...) % mod
+    vector<int64_t> p(text.length());
     p[0] = 1;
-    for(int idx = 1 ; idx < length ; idx++) {
+    for(int idx = 1 ; idx < p.size() ; idx++)
         p[idx] = (p[idx - 1] * base) % mod;
-        cout << p[idx] << endl;
-    }
-    // GENERATING HASHCODES
-    int hashcode[length];
-    for(int idx = 0 ; idx < length ; idx++) {
-        hashcode[idx] = str.at(idx) - 'a' + 1;
-        cout << hashcode[idx] << endl;
-    }
-    
-    // FINDING PREFIX AND SUFFIX
-    int i = 0, j = length - 1, prefix = 0, suffix = 0;
-    for(int iter = 0 ; iter < length - 1 ; iter++) {
-        int64_t prefix = prefix + hashcode[i] * p[iter];
-        int64_t suffix = suffix * base + hashcode[j];
 
-        cout << prefix << "\t" << suffix << endl;
-        if(prefix == suffix)
-            cout << i << " , " << j << endl;
+    vector<int64_t> thash(text.length() + 1, 0);
+    for(int idx = 0 ; idx < text.length() ; idx++)
+        thash[idx + 1] = (thash[idx] + (text.at(idx) - 'a' + 1) * p[idx]) % mod;
+
+    string ans = "Just a legend";
+    // CHECKING FOR SIMILARITY IN PREFIX AND SUFFIX
+    for(int len = text.length() - 2 ; len >= 1 ; len -- ) {
+        int64_t phash = thash[len];
+        int64_t shash = (thash[text.length()] + mod - thash[text.length() - len]) % mod;
+
+        if( (phash * p[text.length() - len]) % mod == shash ) {
+            // HERE WE GOT MATCHED PREFIX AND SUFFIX
+            // USING PHASH ON ROLLING IT OVER THE IN BETWEEN STRING 
+            for(int idx = 2 ; idx + len <  thash.size()  ; idx++) {
+                int64_t chash = (thash[ idx + len - 1] + mod - thash[ idx - 1 ]) % mod;
+                if( chash == (phash * p[idx - 1]) % mod) {
+                    ans = text.substr(0, len);
+                    cout << ans;
+                    return 0;                
+                }
+            }
+        }
     }
 
+    cout << ans;
+    return 0;
 }
